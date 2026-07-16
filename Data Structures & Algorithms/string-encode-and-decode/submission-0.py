8a9fb36e24514dce23db20b098a93f6d6c5b66ea
sofyapzn-web <sofya.pzn@gmail.com>
2026-07-16 19:03:16 +0300

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        sol = ''
+        for s in strs :
+            sol += len(s) + s
+        return sol
+
+    def decode(self, s: str) -> List[str]:
+        l = []
+        while s!='':
+            l.append(s[1, s[0]+2])
+            s = s[s[0]+1:]
+        return l
