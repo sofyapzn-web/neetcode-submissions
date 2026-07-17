d1e1a1f3e6e20502f09505edd7cfb7cb396992da
sofyapzn-web <sofya.pzn@gmail.com>
2026-07-17 21:06:34 +0300

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        r = len(nums)
+        cnt = 0
+        d = defaultdict(list)
+        for i in range(r):
+            for j in range(r):
+                for k in range(r):
+                    ni = nums[i]
+                    nj=nums[j]
+                    nk = nums[k]
+                    if ni!=nj and nj!=nk and ni+nj+nk==0:
+                        d[cnt].append([nj,ni,nk])
+                        cnt+=1
+        s = []
+        for x in d.values():
+            sx = sorted(x)
+            if sx not in s :
+                s.append(sx)
+        return s
