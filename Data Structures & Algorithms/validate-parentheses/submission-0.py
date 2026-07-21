2c6490d95edf2d753d37395e71d9c7123ff1e766
sofyapzn-web <sofya.pzn@gmail.com>
2026-07-21 16:33:24 +0300

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        CTO = {')':'(', '}':'{', ']':'['}
+        stack =[]
+        for ss in s:
+            if ss in CTO :
+                if stack and stack[-1]==CTO[ss]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(ss)
+        return True
+            
+
+
+
+
+
+
